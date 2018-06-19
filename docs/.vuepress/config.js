ccd18ef9c4a31f756a229e02770993307611f32c
zy <zy450821@163.com>
2018-06-19 14:28:05 +0800

    初始化项目

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: "zyuan's docs",
+  description: 'front-end docs',
+  base: '/',
+  port: 9001,
+  themeConfig: {
+    nav: [
+      {text: 'Home', link: '/'}
+    ]
+  }
+}
---
+++ docs/README.md
@@ -0,0 +1,5 @@
+# my docs
+
+---
+sidebar: auto
+---
